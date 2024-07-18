0966af9d2959becb9118d9f45d92f699adc9fed9
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-07-18 17:51:52 +0530

    db added

--- railway/settings.py
+++ railway/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = "railway.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'railway',
-#         'USER': 'root',
-#         'PASSWORD': 'SptQBNoGrjqSHQDJHnzlAjBwyPPbfcrN',
-#         'HOST': 'viaduct.proxy.rlwy.net',
-#         'PORT': '45959',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD': 'xkxmxuMAXAGgtJgejSKObzlgWwMnpQIL',
+       'HOST': 'roundhouse.proxy.rlwy.net',
+       'PORT': '42463',
+   }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
