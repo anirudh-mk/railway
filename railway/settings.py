0ab4603057f0af8b63b0f7b0d10122f1231c8434
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-07-18 17:08:45 +0530

    feat(database added)

--- railway/settings.py
+++ railway/settings.py
@@ -74,14 +74,23 @@ WSGI_APPLICATION = "railway.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'SptQBNoGrjqSHQDJHnzlAjBwyPPbfcrN',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '45959',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
